d9479d4ff9789631a4fcefe4eefab6f414e002a4
Kortsa <leoklomegah8@gmail.com>
2025-02-11 12:56:55 +0000

    added functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,39 @@ import { useState } from "react";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilteredButton from "./components/FilterButton";
+import { nanoid } from "nanoid";
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
 
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
   function addTask(name) {
-    const newTask = { id: "id", name, completed: false };
-    setTasks([...tasks, newTask])
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, name: newName };
+      }
+      return task
+    });
+    setTasks(editedTaskList)
   }
 
   const taskList = tasks?.map((task) => (
@@ -17,8 +43,15 @@ function App(props) {
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
     />
   ));
+
+  const tasksNoun = taskList.length <= 1 ? "task" : "tasks";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -29,7 +62,7 @@ function App(props) {
         <FilteredButton />
         <FilteredButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
